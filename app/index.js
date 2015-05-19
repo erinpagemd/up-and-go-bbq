bd31ef0ebeb515515da33c6d4ebb8774aabcc441
YOUR NAME <erinpagemd@yahoo.com>
2015-05-18 23:10:05 -0500

    add in a schedule view

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ angular
   $stateProvider
     .state('home', {url:'/', templateUrl:'views/home.html'})
     .state('menu', {url:'/menu', templateUrl:'views/menu.html'})
+    .state('schedule', {url:'/schedule', templateUrl:'views/schedule.html'})
     .state('contact', {url:'/contact', templateUrl:'views/contact.html'})
 
 });
